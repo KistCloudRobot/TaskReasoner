299b4167a3b006006c3771d45a1881db8d184c34
DonggiKim <7979us@gmail.com>
2021-12-15 19:20:25 +0900

    delete old plan ignore debugfile

--- TaskReasoner_Local/src/kr/ac/uos/ai/robot/intelligent/taskReasoner/TaskReasoner_Local.java
+++ TaskReasoner_Local/src/kr/ac/uos/ai/robot/intelligent/taskReasoner/TaskReasoner_Local.java
@@ -42,7 +42,7 @@ public class TaskReasoner_Local extends ArbiAgent {
 	public static String ENV_ROBOT_NAME;
 	public static final String ARBI_PREFIX = "www.arbi.com/";
 
-	private static String brokerURI = "tcp://172.16.165.204:8000";
+	private static String brokerURI = "tcp://172.16.165.171:61313";
 	private static String TASKREASONER_ADDRESS;
 	private static int brokerType = 2;
 	private static String TASKMANAGER_ADDRESS;
@@ -99,7 +99,7 @@ public class TaskReasoner_Local extends ArbiAgent {
 		//ENV_AGENT_NAME = System.getenv("AGENT");
 		//ENV_ROBOT_NAME = System.getenv("ROBOT");
 		
-		ENV_JMS_BROKER = "tcp://192.168.0.14" + ":61313";
+		ENV_JMS_BROKER = "tcp://172.16.165.171:61313";
 		ENV_AGENT_NAME = "Local";
 
 		TASKMANAGER_ADDRESS = ARBI_PREFIX + ENV_AGENT_NAME + "/TaskManager";
